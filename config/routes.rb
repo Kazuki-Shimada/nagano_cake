3b1bfa565768d31bf9786e2bbf68cee83d08044c
EC2 Default User <ec2-user@ip-172-31-32-178.ap-northeast-1.compute.internal>
2022-12-22 14:58:04 +0000

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
+  
+  namespace :public do
+    get 'customers/show'
+    get 'customers/edit'
+    get 'customers/confirm'
+  end
+  devise_for :customers
+  
+  root to: "public/homes#top"
+  
+  namespace :public do
+    get 'about' => 'homes#about'
+    resources :items, only: [:index, :show]
+    resources :customers, only: [:show, :edit, :update, :confirm, :withdrawal]
+  end
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
