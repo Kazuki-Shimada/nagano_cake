677b706e5cb446b1bf05fd5bbcfe69a753e6babe
EC2 Default User <ec2-user@ip-172-31-32-178.ap-northeast-1.compute.internal>
2022-12-25 14:51:31 +0000

    cart_items

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
     resources :items, only: [:index, :show]
     resources :customers, only: [:show, :edit, :update, :confirm, :withdrawal, :update]
     resources :addresses, only: [:index, :edit, :create, :update, :destroy]
+    resources :cart_items, only: [:index, :update, :destroy, :destroy_all, :create]
   end
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
