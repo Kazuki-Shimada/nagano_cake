a81e53306b5ed099f791fa6dceadc6ce875f36fc
EC2 Default User <ec2-user@ip-172-31-32-178.ap-northeast-1.compute.internal>
2023-01-14 13:28:30 +0000

    cart_item

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,6 +1,25 @@
 class Public::CartItemsController < ApplicationController
   def index
     @cart_items = CartItem.all
-    @cart_item = CartItem.find(params[:])
+  end
+  def create
+    @cart_item = CartItem.new(cart_item_params)
+    @cart_item.customer_id = current_customer.id
+
+    if @cart_item.save
+      flash[:notice] ="カートに追加しました"
+      redirect_to public_cart_items_path
+    else
+      render :index
+    end
+  end
+  def destroy_all
+    @destroy_all = current_customer.destroy_all
+  end
+
+  private
+
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :amount)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -13,4 +13,7 @@ class Item < ApplicationRecord
       end
       image.variant(resize_to_limit: [width, height]).processed
     end
+    def with_tax_price
+      (price * 1.1).floor
+    end
 end
